5eb11e2a9675eb9eb915668bcfe24c3e3891f0c6
alyStrom5062 <Alyenastrom00@gmail.com>
2023-06-19 03:01:11 -0700

    Delete functionality implemented

--- zzPractice/restrant/controllers/places.js
+++ zzPractice/restrant/controllers/places.js
@@ -28,6 +28,21 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+// DELETE
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]){
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
 // SHOW
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -38,7 +53,7 @@ router.get('/:id', (req, res) => {
       res.render('error404')
     }
     else {
-        res.render('places/show', { place: places[id] })
+        res.render('places/show', { place: places[id], id })
     }
   })
 
--- zzPractice/restrant/views/places/show.jsx
+++ zzPractice/restrant/views/places/show.jsx
@@ -16,6 +16,12 @@ function Show (data) {
           <h3>Comments</h3>
           <p>No comments yet!</p>
 
+          <a href={`/places/${data.id}/edit`} className='btn btn-warning'>Edit</a>
+
+          <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+            <button type='submit' className='btn btn-danger'>Delete</button>
+          </form>
+
           </main>
         </Def>
     )
